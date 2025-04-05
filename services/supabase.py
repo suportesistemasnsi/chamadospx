4cf582a3324ed92c17c5ba9027f7c69165b2eb0b
suportesistemasnsi <suportesistemasnsi@gmail.com>
2025-04-05 09:33:44 -0300

    Update supabase.py

--- services/supabase.py
+++ services/supabase.py
@@ -14,7 +14,7 @@ if not SUPABASE_URL or not SUPABASE_KEY:
 # Criar cliente Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@st.cache_data
+st.cache_data(ttl=300) 
 def carregar_chamados():
     """Função para carregar os chamados do banco de dados"""
     try:
@@ -35,4 +35,32 @@ def carregar_chamados():
         return pd.DataFrame()
     except Exception as e:
         st.error(f"Erro ao carregar os chamados: {e}")
+        return pd.DataFrame()
+    
+@st.cache_data(ttl=300) 
+def carregar_chamados_fc():
+    """Função para carregar os chamados do banco de dados"""
+    try:
+        response = supabase.table("Chamados_fc").select("*").execute()
+        if response.data:
+            df = pd.DataFrame(response.data)
+            df.rename(columns={
+                "chamado_sd": "Chamados SH",
+                "chamado_facil": "Chamados Fácil",
+                "titulo": "Título",
+                "data_abertura": "Data",
+                "pendencia_retorno": "Pendência",
+                "usuario_resp": "Usuário Resp",
+                "status": "Status",
+                "observacao": "Observação",
+            }, inplace=True)
+
+            # Formatação opcional
+            df["Data"] = pd.to_datetime(df["Data"], errors="coerce")
+            return df
+        else:
+            st.warning("Nenhum dado retornado da tabela Chamados_fc.")
+            return pd.DataFrame()
+    except Exception as e:
+        st.error(f"Erro ao carregar chamados Fácil: {str(e)}")
         return pd.DataFrame()
